c5a7acb89e4f47cffb35b0ba4b8e160ef2db4594
Sn0wCrack <442287+Sn0wCrack@users.noreply.github.com>
2026-06-28 11:43:27 +1000

    fix: use wget here too

--- create-ubuntu-bootstraps.sh
+++ create-ubuntu-bootstraps.sh
@@ -75,7 +75,7 @@ apt-get -y install software-properties-common ca-certificates
 wget -qO- "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC8EC952E2A0E1FBDC5090F6A2C277A0A352154E5" \
   | sudo gpg --batch --yes --dearmor --output "/etc/apt/trusted.gpg.d/ubuntu-toolchain-r_ubuntu_test.gpg"
 
-curl -qO- "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xBBB8BD3BBE6AD3419048EDC50795A9A788A59C82" \
+wget -qO- "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xBBB8BD3BBE6AD3419048EDC50795A9A788A59C82" \
   | sudo gpg --batch --yes --dearmor --output "/etc/apt/trusted.gpg.d/cybermax-dexter_ubuntu_mingw-w64-backport.gpg"
 
 echo "deb https://ppa.launchpadcontent.net/ubuntu-toolchain-r/test/ubuntu $(lsb_release -sc) main" \
