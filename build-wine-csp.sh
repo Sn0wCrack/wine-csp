85a21d6c2443e919e7b5587ff47b74388a9b5b2f
Sn0wCrack <442287+Sn0wCrack@users.noreply.github.com>
2026-06-30 08:40:12 +1000

    fix: just use fallback for unbound

--- build-wine-csp.sh
+++ build-wine-csp.sh
@@ -7,11 +7,7 @@ if [ -z "${WINE_VERSION}" ]; then
     exit 1
 fi
 
-if [ -z "${XDG_CACHE_HOME}" ]; then
-	export XDG_CACHE_HOME="${HOME}"/.cache
-fi
-
-mkdir -p "${XDG_CACHE_HOME}"/ccache
+mkdir -p "${XDG_CACHE_HOME:-${HOME}/.cache}"/ccache
 mkdir -p "${HOME}"/.ccache
 
 JOB_COUNT=$(($(getconf _NPROCESSORS_ONLN) + 2))
@@ -85,7 +81,7 @@ build_with_bwrap () {
     bwrap --ro-bind "${BOOTSTRAP_PATH}" / --dev /dev --ro-bind /sys /sys \
 		  --proc /proc --tmpfs /tmp --tmpfs /home --tmpfs /run --tmpfs /var \
 		  --tmpfs /mnt --tmpfs /media --bind "${BUILD_DIR}" "${BUILD_DIR}" \
-		  --bind-try "${XDG_CACHE_HOME}"/ccache "${XDG_CACHE_HOME}"/ccache \
+		  --bind-try "${${XDG_CACHE_HOME:-${HOME}/.cache}}"/ccache "${${XDG_CACHE_HOME:-${HOME}/.cache}}"/ccache \
 		  --bind-try "${HOME}"/.ccache "${HOME}"/.ccache \
           --setenv PATH "/opt/mingw/x86_64/bin:/opt/mingw/i686/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin" \
 		  "$@"
